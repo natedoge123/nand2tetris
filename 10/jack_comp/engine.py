5802f9cb82f193e56f8144d6476534b02021bc8c
natedoge123 <98nhartley@gmail.com>
2024-05-21 20:57:21 -0400

    working on chapter 10

--- 10/jack_comp/engine.py
+++ 10/jack_comp/engine.py
@@ -1,59 +1,36 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-def runEngine(token): #takes tokenized xml tree and gives foramtted one
+def runEngine(xml): #takes tokenized xml tree and gives foramtted one
+    new_xml_tree = ET.Element('class')
+    programStructure(xml)
+
+
+    return new_xml_tree
+
+def programStructure(xml):
+    program_struct_counts = programStructureCounter(xml)
+
+    return program_struct_counts
+
+def programStructureCounter(xml):
+    static_list = []
+    field_list = []
+
+    for elem in xml:
+        if (elem.text == 'static'):
+            static_list.append(elem.text)
+
+        if (elem.text == 'field'):
+            field_list.append(elem.text)
     
-    return 0
-
-def compClass():
-    return 0
-
-def compClassVarDec():
-    return 0
-
-def compSubRoutine():
-    return 0
-
-def compParameterList():
-    return 0
-
-def compVarDec():
-    return 0
-
-def compStatement():
-    return 0
-
-def compDo():
-    return 0
-
-def compLet():
-    return 0
-
-def compWhile():
-    return 0
-
-def compReturn():
-    return 0
-
-def compIf():
-    return 0
-
-def compExpression():
-    return 0
-
-def compTerm():
-    return 0
-
-def compExpressionList():
-    return 0
+    print ([static_list, field_list])
+    return ([static_list, field_list])
 
 
 def xmlPrint(xml):
     long_string = ET.tostring(xml, 'utf-8')
     indented = minidom.parseString(long_string)
     return indented.toprettyxml(indent="    ")
-
-
-
 
 
--- 10/jack_comp/main.py
+++ 10/jack_comp/main.py
@@ -1,25 +1,31 @@
 import files
-import compliation
 import myToken
+import engine
 
 import sys
-from pathlib import Path
 
 
 file_list = files.open_dir(sys.argv[1])
 
 for file in file_list:
+    #   Getting the file name right
     file_name = str(file).split('/')[-1]
     file_name = file_name.split('.')[0]
-    print(file_name)
+    #print(file_name)
+
+    #   Tokenize file 
     file_string = files.read_files(file)
     tokenized = (myToken.tokenizer(file_string))
     #print(tokenized)
     xmled = myToken.xmlToken(tokenized)
 
-    write_name = sys.argv[1] + "/" +file_name + "TT"
-    print(write_name)
+    #   Format the tokenized xml to the correcto output
+    formatted_xml = engine.runEngine(xmled)
+    print(formatted_xml)
 
+
+    #   Save the files
+    write_name = sys.argv[1] + "/" +file_name + "TT"
     files.write_file(str(xmled), write_name, 'xml')
 
 
