6df5f0749563f7ad7721c4a4111807e90ea17317
natedoge123 <98nhartley@gmail.com>
2024-05-26 12:14:42 -0400

    working on chapter 10 project

--- 10/jack_comp/engine.py
+++ 10/jack_comp/engine.py
@@ -3,19 +3,57 @@ from xml.dom import minidom
 
 def run(xml): #takes tokenized xml tree and gives foramtted one
     new_xml_tree = ET.Element('class')
+    count_para = 0
+    count_braces = 0
+    count_bracket = 0
 
-    for i, elem in enumerate(xml):
+    for elem in xml:
 
-        if elem.tag == 'keyword' and elem.text == 'class':
-            temp = ET.SubElement(new_xml_tree, elem.tag)
-            temp.text = elem.text
-            
-            temp1 = ET.SubElement(new_xml_tree, elem[i+1].tag)
-            temp1.text = elem.text
+        if elem.text in ['static', 'field']:
+            sub_routine = ET.SubElement(new_xml_tree, 'classVarDec')
 
+        elif elem.text in ['constructor', 'function', 'method']:
+            sub_routine = ET.SubElement(new_xml_tree, 'subroutineDec')
+            parameter_list = ET.SubElement(sub_routine, 'parameterList')
+            subroutine_body = ET.SubElement(sub_routine, 'subroutineBody')
+            var_dec = ET.SubElement(sub_routine, 'varDec')
 
+        elif elem.text in ['let', 'if', 'else', 'while', 'do', 'return']:
 
-    return new_xml_tree
+            if elem.text == 'let':
+                print(elem.text)
+
+            elif elem.text == 'if':
+                print(elem.text)
+
+            elif elem.text == 'else':
+                print(elem.text)
+
+            elif elem.text == 'while':
+                print(elem.text)
+
+            elif elem.text == 'do':
+                print(elem.text)
+
+            elif elem.text == 'return':
+                print(elem.text)
+
+        elif elem.tag in ['integerConstant', 'stringConstant', 'keywordConstant']:
+            print(elem.text)
+
+        else:
+
+            if elem.text == "(": count_para += 1
+            if elem.text == "{": count_braces += 1
+            if elem.text == "[": count_para += 1
+
+            if elem.text == ")": count_para -= 1
+            if elem.text == "}": count_braces -= 1
+            if elem.text == "]": count_para -= 1
+
+            print('cringe')
+
+    return xmlPrint(new_xml_tree)
 
 
 def addParentToTag(root, tag, new_parent):
