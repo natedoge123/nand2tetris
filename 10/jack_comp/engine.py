8a84ef3cfe0ba69dec6d4d22fd655fe08e3504e0
natedoge123 <98nhartley@gmail.com>
2024-06-09 16:48:05 -0400

    still re working the tree parsing

--- 10/jack_comp/engine.py
+++ 10/jack_comp/engine.py
@@ -152,11 +152,6 @@ def run(xml): #takes tokenized xml tree and gives foramtted one
         token_count += 1
     return xmlPrint(new_xml_tree)
 
-def xmlPrint(xml):
-    long_string = ET.tostring(xml, 'utf-8')
-    indented = minidom.parseString(long_string)
-    return indented.toprettyxml(indent="    ")
-
 def run2(xml):
     new_xml_tree = ET.Element("class")
     main_count = 0
@@ -175,7 +170,7 @@ def run2(xml):
                     temp.text = elem.text
 
                 if (cond_0 and elem.text == "{"):
-                    end_prev_parent = count_1
+                    end_prev_parent = count_1 + 1
                     break
 
                 count_1 += 1
@@ -192,53 +187,57 @@ def run2(xml):
                     temp.text = elem.text
 
                 if (cond_0 and elem.text == ";"):
-                    end_prev_parent = count_1
+                    end_prev_parent = count_1 + 1 
                     break
 
                 count_1 += 1
 
         elif token.text in ['constructor', 'function', 'method']:
-            temp_tree = ET.Element('temp')
-            brace_count = 0
-            last_brace_count = 0
+            #sub_routine = ET.SubElement(new_xml_tree, 'subroutineDec')
+            temp_tree = ET.Element('subroutineDec')
+            #   Main cond to check if in right part of loop
             count_1 = 0
-                
+            count_braces = 0
+            last_count_braces = count_braces
+
             for elem in xml:
-                #   Main cond to check if in right part of loop
                 cond_0 = (count_1 >= main_count) and (count_1 >= end_prev_parent)
-                cond_1 = brace_count < last_brace_count
 
                 if (cond_0):
-                    if elem.text == "{":
-                        brace_count += 1
 
-                    if elem.text == "}":
-                        brace_count -= 1
+                    if (elem.text == "{"):
+                        count_braces += 1
+                    if (elem.text == "}"):
+                        count_braces -= 1
 
-                if (brace_count > 0 and cond_0 ):
                     temp = ET.SubElement(temp_tree, elem.tag)
                     temp.text = elem.text
 
-                if (brace_count == 0 and cond_0 and cond_1):
-                    print(xmlPrint(temp_tree))
-                    sub = subroutine_maker(temp_tree)
-                    append_place = new_xml_tree.find('class')
-                    print(append_place, 'here')
-
-                    break
-                
+                    if ((count_braces == 0) and (last_count_braces > count_braces)):
+                        print(xmlPrint(temp_tree))
+                        sub = subroutine_maker(temp_tree)
+                        end_prev_parent = count_1 + 1
+                        break
+                    last_count_braces = count_braces
                 count_1 += 1
-                last_brace_count = brace_count
-                print(brace_count, count_1)
 
-
-        main_count += 1
     return new_xml_tree
 
 def subroutine_maker(xml):
-    sub_tree = ET.Element
+    sub_tree = ET.Element('subroutineDec')
+    token_count = 0
+
+    for token in xml:
+        token_count += 1
+
+        if ():
+
     return sub_tree
 
+def xmlPrint(xml):
+    long_string = ET.tostring(xml, 'utf-8')
+    indented = minidom.parseString(long_string)
+    return indented.toprettyxml(indent="    ")
 
 
 
