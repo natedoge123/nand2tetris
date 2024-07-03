4a9ae30a6b80d74e575cd464a36aae5425b76122
natedoge123 <98nhartley@gmail.com>
2024-07-02 21:50:49 -0400

    still reworking chapter 10 jack engine to easier to work with. Working on subroutinedec part of the loop and getting the parameter list and subroutinebody apart from the xml that gets passed into the function :

--- 10/jack_comp/enginetwo.py
+++ 10/jack_comp/enginetwo.py
@@ -1,8 +1,9 @@
+from re import sub
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
 def classMaker(xml):
-    class_tree = ET.Element("class")
+    class_tree = ET.Element('class')
     temp_tree = ET.Element('temp')
     brace_count = 0
     last_brace_count = brace_count
@@ -15,66 +16,130 @@ def classMaker(xml):
         if (item.text == '}'):
             brace_count -= 1
 
-        print(brace_count)
-
         if (brace_count == 0 and last_brace_count == 0):
             temp = ET.SubElement(class_tree, item.tag)
             temp.text = item.text
 
-        elif(brace_count == 0 and last_brace_count == 1):
-            class_tree.append(classVarDec(temp_tree))
-            class_tree.append(subroutineDec(temp_tree))
+        elif (brace_count == 0 and last_brace_count == 1):
+            class_var_dec_list = classVarDecFinder(temp_tree)
+            subroutine_dec_list = subrountineFinder(temp_tree)
+
+            for xml_tree in class_var_dec_list:
+                class_tree.append(xml_tree)
+
+            for xml_tree in subroutine_dec_list:
+                class_tree.append(xml_tree)
 
             temp = ET.SubElement(class_tree, item.tag)
             temp.text = item.text
 
-        elif (brace_count != 0):
+        elif (brace_count != 0 and last_brace_count !=0):
             temp = ET.SubElement(temp_tree, item.tag)
             temp.text = item.text
 
+        elif (brace_count == 1 and last_brace_count == 0):
+            temp = ET.SubElement(class_tree, item.tag)
+            temp.text = item.text
 
-    print(xmlPrint(class_tree))
+
+    #print(xmlPrint(class_tree))
     return class_tree
 
-def classVarDec(xml):
-    class_var_dec = ET.Element('classVarDec')
+def classVarDecFinder(xml):
+    xml_list = []
+    temp_xml = ET.Element('temp')
+
     var_dec_active = False
+    brace_count = 0
 
     for item in xml:
+
+        if item.text == '{':
+            brace_count += 1
+        if item.text == '}':
+            brace_count -= 1
+
         if item.text in ['static', 'field']:
+            temp_xml = ET.Element('classVarDec')
             var_dec_active = True
 
         if var_dec_active:
-            temp = ET.SubElement(class_var_dec, item.tag)
+            temp = ET.SubElement(temp_xml, item.tag)
             temp.text = item.text
 
-        if item.text == ';':
+        if item.text == ';' and var_dec_active:
+            xml_list.append(temp_xml)
             var_dec_active = False
 
-    return class_var_dec
+    return xml_list
 
 def subrountineFinder(xml):
-    subroutine_dec = ET.Element('subroutineDec')
+    xml_list = []
+    temp_xml = ET.Element('subroutineDec')
     sub_active = False
 
+    brace_count = 0
+
     for item in xml:
+        last_brace_count = brace_count
+
         if item.text in ['constructor', 'function', 'method']:
-            sub_active = True
+            temp_xml = ET.Element('temp')
             brace_count = 0
             last_brace_count = brace_count
+            sub_active = True
 
-        if sub_active:
-            temp = ET.SubElement(subroutine_dec, item.tag)
+        if item.text == '{':
+            brace_count += 1
+        if item.text == '}':
+            brace_count -= 1
+
+        if (sub_active):
+            temp = ET.SubElement(temp_xml, item.tag)
+            temp.text = item.text
+            
+        if ((brace_count == 0) and (last_brace_count == 1)):
+            xml_list.append(subroutineDecBuilder(temp_xml))
+            sub_active = False
+
+    return xml_list
+
+def subroutineDecBuilder(xml):
+    print(xmlPrint(xml))
+    temp_xml = ET.Element('subroutineDec')
+    para_list = ET.Element('parameterList')
+    subroutine_body = ET.Element('subroutine_body')
+
+    para_count = 0
+    last_para_count = para_count
+
+    para_list_done = False
+    
+
+    for item in xml:
+        last_para_count = para_count
+
+        if item.text == '(':
+            para_count += 1
+        if item.text == ')':
+            para_count -= 1
+
+        if (para_count == 0 and last_para_count == 0 and para_list_done = False):
+            temp = ET.SubElement(temp_xml, item.tag)
             temp.text = item.text
 
-    return subroutine_dec
 
-def subroutineDec(xml):
-    temp = ET.Element('temp')
+        if (para_count == 0 and last_para_count == 0):
+            temp = ET.SubElement(temp_xml, item.tag)
+            temp.text = item.text
 
-    return temp
+        elif (para_count == 0 and last_para_count == 1):
+            temp = ET.SubElement(temp_xml, item.tag)
+            temp.text = item.text
+            
 
 
+    return temp_xml
 
 def xmlPrint(xml):
     long_string = ET.tostring(xml, 'utf-8')
